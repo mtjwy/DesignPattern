f6a9574485cc75357a6339caa589e1231775954a
Yu Wu <wuyucsu@gmail.com>
2015-06-01 18:43:22 -0500

    Implement a new Java I/O Decorator to convert all uppercase char to lowercase char

---
+++ DecoratorPattern/src/main/java/newJavaIO_Decorator/LowerCaseInputStream.java
@@ -0,0 +1,27 @@
+package newJavaIO_Decorator;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LowerCaseInputStream extends FilterInputStream {
+
+	protected LowerCaseInputStream(InputStream in) {
+		super(in);
+	}
+	
+	public int read() throws IOException {
+		int c = super.read();
+		return (c == -1 ? c : Character.toLowerCase((char) c));
+	}
+	
+	public int read(byte[] b, int offset, int len) throws IOException {
+		int result = super.read(b, offset, len);
+		for (byte bb : b) {
+			bb = (byte)Character.toLowerCase((char)bb);
+		}
+		return result;
+	}
+	
+	
+}
