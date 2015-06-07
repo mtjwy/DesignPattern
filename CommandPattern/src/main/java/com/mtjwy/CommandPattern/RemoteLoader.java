43e01b1b5f04e53108d55d20f700a154d8c53b81
Yu Wu <wuyucsu@gmail.com>
2015-06-07 12:32:11 -0500

    test Macro Command in RemoteLoader

--- CommandPattern/src/main/java/com/mtjwy/CommandPattern/RemoteLoader.java
+++ CommandPattern/src/main/java/com/mtjwy/CommandPattern/RemoteLoader.java
@@ -50,5 +50,36 @@ public class RemoteLoader {
 		System.out.println("--- Pushing Undo ---");
 		remoteControl.undoButtonWasPushed();
 		
+		//test Macro
+		System.out.println();
+		System.out.println();
+		
+		TV tv = new TV("Living Room");
+		Stereo stereo = new Stereo("Living Room");
+		Hottub hottub = new Hottub();
+ 
+		
+		StereoOnCommand stereoOn = new StereoOnCommand(stereo);
+		TVOnCommand tvOn = new TVOnCommand(tv);
+		HottubOnCommand hottubOn = new HottubOnCommand(hottub);
+		
+		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+		TVOffCommand tvOff = new TVOffCommand(tv);
+		HottubOffCommand hottubOff = new HottubOffCommand(hottub);
+		
+		
+		Command[] partyOn = { lightOn, stereoOn, tvOn, hottubOn};
+		Command[] partyOff = { lightOff, stereoOff, tvOff, hottubOff};
+  
+		MacroCommand partyOnMacro = new MacroCommand(partyOn);
+		MacroCommand partyOffMacro = new MacroCommand(partyOff);
+ 
+		remoteControl.setCommand(0, partyOnMacro, partyOffMacro);
+  
+		System.out.println(remoteControl);
+		System.out.println("--- Pushing Macro On---");
+		remoteControl.onButtonWasPushed(0);
+		System.out.println("--- Pushing Macro Off---");
+		remoteControl.offButtonWasPushed(0);
 	}
 }
