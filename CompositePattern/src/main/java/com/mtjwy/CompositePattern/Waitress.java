c44b4f0d59fbc58ac9ce4fbc8b1047280110fea6
Yu Wu <wuyucsu@gmail.com>
2015-06-12 14:01:14 -0500

    add printVegetarianMenu()

--- CompositePattern/src/main/java/com/mtjwy/CompositePattern/Waitress.java
+++ CompositePattern/src/main/java/com/mtjwy/CompositePattern/Waitress.java
@@ -1,5 +1,7 @@
 package com.mtjwy.CompositePattern;
 
+import java.util.Iterator;
+
 public class Waitress {
 	MenuComponent allMenus;
  
@@ -9,6 +11,23 @@ public class Waitress {
  
 	public void printMenu() {
 		allMenus.print();
+	}
+	
+	public void printVegetarianMenu() {
+		Iterator<MenuComponent> iterator = allMenus.createIterator();
+		System.out.println("\nVEGETARIAN MENU\n----");
+		
+		while (iterator.hasNext()) {
+			MenuComponent menuComponent = 
+					(MenuComponent) iterator.next();
+			try {
+				if (menuComponent.isVegetarian()) {
+					menuComponent.print();
+				}
+			} catch (UnsupportedOperationException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
 
